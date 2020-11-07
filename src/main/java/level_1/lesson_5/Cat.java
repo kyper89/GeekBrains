4f2d36b68841ac38e66763512feb8334e15db7a3
kyper89 <kyper89@mail.ru>
2020-11-07 22:30:15 +0300

    Lesson for and five

---
+++ src/main/java/level_1/lesson_5/Cat.java
@@ -0,0 +1,21 @@
+package level_1.lesson_5;
+
+public class Cat {
+
+    String name;
+    String color;
+    int age;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name, String color, int age) {
+
+        this.name = name;
+        this.color = color;
+        this.age = age;
+
+    }
+
+}
---
+++ src/main/java/level_1/lesson_5/CatDemoApp.java
@@ -0,0 +1,9 @@
+package level_1.lesson_5;
+
+public class CatDemoApp {
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Барсик", "Белый" ,4);
+        Cat cat2 = new Cat("Мурзик", "Черный", 6);
+    }
+}
