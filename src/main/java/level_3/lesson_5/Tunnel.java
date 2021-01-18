0da4e10539359524e01ef591f60a620b54db0874
kyper89 <kyper89@mail.ru>
2021-01-18 22:36:00 +0300

    Lesson five homework

--- src/main/java/level_3/lesson_5/Tunnel.java
+++ src/main/java/level_3/lesson_5/Tunnel.java
@@ -1,7 +1,12 @@
 package level_3.lesson_5;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+    private final Semaphore smp;
+    public Tunnel(Semaphore smp) {
+        this.smp = smp;
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
@@ -9,13 +14,17 @@ public class Tunnel extends Stage {
     public void go(Car c) {
         try {
             try {
-                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                if (!smp.tryAcquire(1, TimeUnit.NANOSECONDS)) {
+                    System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                    smp.acquire();
+                }
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
